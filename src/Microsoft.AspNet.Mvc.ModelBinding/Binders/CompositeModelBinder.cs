8567608b12a06a1cefabe7cfa89e974a70a9d4ff
Doug Bunting <dougbu@microsoft.com>
2014-12-02 11:18:42 -0800

    PR comment; remove a space

--- src/Microsoft.AspNet.Mvc.ModelBinding/Binders/CompositeModelBinder.cs
+++ src/Microsoft.AspNet.Mvc.ModelBinding/Binders/CompositeModelBinder.cs
@@ -151,7 +151,7 @@ namespace Microsoft.AspNet.Mvc.ModelBinding
                 // ValueProvider property might contain a filtered list of value providers.
                 // While deciding to bind a particular property which is annotated with a IValueProviderMetadata,
                 // instead of refiltering an already filtered list, we need to filter value providers from a global
-                // list  of all value providers. This is so that every artifact that is explicitly marked using an
+                // list of all value providers. This is so that every artifact that is explicitly marked using an
                 // IValueProviderMetadata can restrict model binding to only use value providers which support this
                 // IValueProviderMetadata.
                 var valueProvider =
